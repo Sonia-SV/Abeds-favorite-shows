9f7d9450bf4b9501bf03bfe078326d899fe294b4
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-07-09 20:36:40 +0200

    Listen button and API fech

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-console.log('>> Ready :)');
+//Arrays
+
+let searchShows = [];
+let favShows = [];
+
+//Const
+const search = document.querySelector('.js-search');
+
+//handle events
+const searchButton = document.querySelector('.js-search-button');
+
+function handleSearchButton(ev) {
+  ev.preventDefault();
+  getShowsFromApi();
+}
+
+//Api
+
+const getShowsFromApi = () => {
+  let searchShow = search.value;
+  fetch(`http://api.tvmaze.com/search/shows?q=${searchShow}`)
+    .then((response) => response.json())
+    .then((searchData) => {
+      console.log(searchData);
+      for (const data of searchData) {
+        paintSearchShows(data);
+      }
+    });
+};
+
+//Paint
+
+const paintSearchShows = (ev) => {
+  console.log(ev.show);
+  console.log(ev.show.id);
+  console.log(ev.show.name);
+  console.log(ev.show.image.medium);
+};
+getShowsFromApi();
+
+searchButton.addEventListener('click', handleSearchButton);
