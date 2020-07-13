78d3b2f1016d5c5d03e2d068e4b301bcf350dc7b
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-07-13 11:24:01 +0200

    Simplify DOM

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -2,65 +2,14 @@
 //Const
 const formSubmit = document.querySelector('.js-form');
 const searchInput = document.querySelector('.js-search');
-const mainPage = document.querySelector('.js-main');
+
+const favList = document.querySelector('.js-fav-list');
+const searchList = document.querySelector('.js-search-list');
+const tvFav = document.querySelector('.js-tv-fav');
 const searchButton = document.querySelector('.js-search-button');
-
-const showsContainer = document.createElement('section');
-const searchListContainer = document.createElement('div');
-const favListContainer = document.createElement('aside');
-const searchList = document.createElement('ul');
-const favList = document.createElement('ul');
-const favElement = document.createElement('div');
-const favsWarning = document.createElement('p');
-
-showsContainer.setAttribute('class', 'main__shows');
-searchListContainer.setAttribute('class', 'main__shows__search js-search-results');
-favListContainer.setAttribute('class', 'main__shows__fav wrapper');
-searchList.setAttribute('class', 'main__shows__search__results');
-favList.setAttribute('class', 'main__shows__fav__results__ul');
-mainPage.appendChild(showsContainer);
-showsContainer.appendChild(favListContainer);
-showsContainer.appendChild(searchListContainer);
-
-const tvIcon = document.createElement('div');
-tvIcon.setAttribute('class', 'main__shows__fav__tv  js-tv-icon');
-favListContainer.appendChild(tvIcon);
-const tvFav = document.createElement('p');
-const tvImg = document.createElement('img');
-const tvButton = document.createElement('div');
-tvButton.setAttribute('class', 'main__shows__fav__tv__button js-favs-toggle');
-
-tvImg.setAttribute('class', 'main__shows__fav__tv__img');
-tvImg.setAttribute('src', './assets/images/tv.png');
-
-tvIcon.appendChild(tvImg);
-tvIcon.appendChild(tvFav);
-tvIcon.appendChild(tvButton);
-
-const resetContainer = document.createElement('div');
-const resetImg = document.createElement('img');
-const resetButton = document.createElement('div');
-
-resetButton.setAttribute('class', 'js-button-reset main__shows__fav__reset__button ');
-resetImg.setAttribute('class', 'main__shows__fav__reset__img');
-resetImg.setAttribute('src', './assets/images/trashcan.png');
-
-const reset = document.createTextNode('Reset Favorites');
-resetContainer.setAttribute('class', 'main__shows__fav__reset');
-favListContainer.appendChild(resetContainer);
-resetButton.appendChild(reset);
-resetContainer.appendChild(resetImg);
-resetContainer.appendChild(resetButton);
-
-const errorSearch = document.createElement('p');
-errorSearch.setAttribute('class', 'js-error-message');
-searchListContainer.appendChild(searchList);
-searchListContainer.appendChild(errorSearch);
-
-favElement.setAttribute('class', 'main__shows__fav__results js-fav-results');
-favListContainer.appendChild(favElement);
-favElement.appendChild(favsWarning);
-favElement.appendChild(favList);
+const tvButton = document.querySelector('.js-tv-fav-btn');
+const errorSearch = document.querySelector('.js-error-message');
+const favsWarning = document.querySelector('.js-fav-message');
 
 //Arrays
 
@@ -75,12 +24,6 @@ const handleSearchButton = (ev) => {
   resetSearch();
   getShowsFromApi();
 };
-
-// const handleSubmit = (ev) => {
-//   ev.preventDefault();
-//   resetSearch();
-//   getShowsFromApi();
-// }
 
 //Reset Search
 const resetSearch = () => {
